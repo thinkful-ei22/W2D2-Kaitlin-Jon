95c986d1ea26571ac21f4262de4292c7f85ec106
Kaitlin Bunn <kabunn1991@gmail.com>
2018-06-19 16:29:39 -0400

    Finished project, bonus 1 done

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,179 @@
-const API_KEY = 'YOUR_KEY_HERE';
+'use strict';
+
+const API_KEY = 'AIzaSyAog2cX6A6PK5xVZPSysT9oXMBOOKCWUa0';
+const mockData = {
+  "kind": "youtube#searchListResponse",
+  "etag": "\"DuHzAJ-eQIiCIp7p4ldoVcVAOeY/VlxTh6fCR13VlOAl5zRTGMqPN70\"",
+  "nextPageToken": "CAUQAA",
+  "regionCode": "US",
+  "pageInfo": {
+   "totalResults": 1000000,
+   "resultsPerPage": 5
+  },
+  "items": [
+   {
+    "kind": "youtube#searchResult",
+    "etag": "\"DuHzAJ-eQIiCIp7p4ldoVcVAOeY/ugcdBfzN9HQpX2pj_mcfaWRDHhg\"",
+    "id": {
+     "kind": "youtube#channel",
+     "channelId": "UClOf1XXinvZsy4wKPAkro2A"
+    },
+    "snippet": {
+     "publishedAt": "2014-09-27T06:22:50.000Z",
+     "channelId": "UClOf1XXinvZsy4wKPAkro2A",
+     "title": "PlayOverwatch",
+     "description": "Overwatch™ is a highly stylized team-based shooter set in a future worth fighting for. Every match is an intense multiplayer showdown pitting a diverse cast of ...",
+     "thumbnails": {
+      "default": {
+       "url": "https://yt3.ggpht.com/-M2-DH_DN-Qs/AAAAAAAAAAI/AAAAAAAAAAA/kczw_wtvMOI/s88-c-k-no-mo-rj-c0xffffff/photo.jpg"
+      },
+      "medium": {
+       "url": "https://yt3.ggpht.com/-M2-DH_DN-Qs/AAAAAAAAAAI/AAAAAAAAAAA/kczw_wtvMOI/s240-c-k-no-mo-rj-c0xffffff/photo.jpg"
+      },
+      "high": {
+       "url": "https://yt3.ggpht.com/-M2-DH_DN-Qs/AAAAAAAAAAI/AAAAAAAAAAA/kczw_wtvMOI/s800-c-k-no-mo-rj-c0xffffff/photo.jpg"
+      }
+     },
+     "channelTitle": "PlayOverwatch",
+     "liveBroadcastContent": "upcoming"
+    }
+   },
+   {
+    "kind": "youtube#searchResult",
+    "etag": "\"DuHzAJ-eQIiCIp7p4ldoVcVAOeY/tv7Me-b8SxgnF2QOEsoD18C_j44\"",
+    "id": {
+     "kind": "youtube#video",
+     "videoId": "mIfI-ZEeshQ"
+    },
+    "snippet": {
+     "publishedAt": "2018-06-17T21:25:56.000Z",
+     "channelId": "UC3xbjTJT2pVNR6b94Vaei5g",
+     "title": "Overwatch - The New REBUILT Reinhardt",
+     "description": "Overwatch news reinhardt being rebuilt, tracer's girlfriend emily got a spray, Overwatch team talks LFG & endorsements. RAZER Giveaway ...",
+     "thumbnails": {
+      "default": {
+       "url": "https://i.ytimg.com/vi/mIfI-ZEeshQ/default.jpg",
+       "width": 120,
+       "height": 90
+      },
+      "medium": {
+       "url": "https://i.ytimg.com/vi/mIfI-ZEeshQ/mqdefault.jpg",
+       "width": 320,
+       "height": 180
+      },
+      "high": {
+       "url": "https://i.ytimg.com/vi/mIfI-ZEeshQ/hqdefault.jpg",
+       "width": 480,
+       "height": 360
+      }
+     },
+     "channelTitle": "ohnickel",
+     "liveBroadcastContent": "none"
+    }
+   },
+   {
+    "kind": "youtube#searchResult",
+    "etag": "\"DuHzAJ-eQIiCIp7p4ldoVcVAOeY/j3K4ESWTL36FYhKefUWKGXAHwaI\"",
+    "id": {
+     "kind": "youtube#video",
+     "videoId": "eyTLhI9hNeg"
+    },
+    "snippet": {
+     "publishedAt": "2018-06-15T17:01:29.000Z",
+     "channelId": "UCmSwqv2aPbuOGiuii2TeaLQ",
+     "title": "Overwatch NEW MOIRA & SYMMETRA PTR CHANGES - HUGE 740mb Patch!",
+     "description": "RANK UP FAST! w/ Pursuit Sign up FREE! https://pursuit.gg/invite/blame (Affiliate) Works on PC, Xbox One, and PS4 Pro, check website for specific Instructions.",
+     "thumbnails": {
+      "default": {
+       "url": "https://i.ytimg.com/vi/eyTLhI9hNeg/default.jpg",
+       "width": 120,
+       "height": 90
+      },
+      "medium": {
+       "url": "https://i.ytimg.com/vi/eyTLhI9hNeg/mqdefault.jpg",
+       "width": 320,
+       "height": 180
+      },
+      "high": {
+       "url": "https://i.ytimg.com/vi/eyTLhI9hNeg/hqdefault.jpg",
+       "width": 480,
+       "height": 360
+      }
+     },
+     "channelTitle": "Blame The Controller - Overwatch",
+     "liveBroadcastContent": "none"
+    }
+   },
+   {
+    "kind": "youtube#searchResult",
+    "etag": "\"DuHzAJ-eQIiCIp7p4ldoVcVAOeY/AEl2kXlp0J1QaSKYOzV8jDogpIc\"",
+    "id": {
+     "kind": "youtube#video",
+     "videoId": "aqj3ATCOLqY"
+    },
+    "snippet": {
+     "publishedAt": "2018-06-17T11:24:37.000Z",
+     "channelId": "UCbR7X5PoUku1OKYJZSIQdVQ",
+     "title": "Fastest Shield Generator Ever..?! | Overwatch Daily Moments Ep.443 (Funny and Random Moments)",
+     "description": "Fastest Shield Generator Ever..?! | Overwatch Daily Moments Ep.443 (Funny and Random Moments) ▻Send your Moments Here ...",
+     "thumbnails": {
+      "default": {
+       "url": "https://i.ytimg.com/vi/aqj3ATCOLqY/default.jpg",
+       "width": 120,
+       "height": 90
+      },
+      "medium": {
+       "url": "https://i.ytimg.com/vi/aqj3ATCOLqY/mqdefault.jpg",
+       "width": 320,
+       "height": 180
+      },
+      "high": {
+       "url": "https://i.ytimg.com/vi/aqj3ATCOLqY/hqdefault.jpg",
+       "width": 480,
+       "height": 360
+      }
+     },
+     "channelTitle": "Daily Overwatch Moments",
+     "liveBroadcastContent": "none"
+    }
+   },
+   {
+    "kind": "youtube#searchResult",
+    "etag": "\"DuHzAJ-eQIiCIp7p4ldoVcVAOeY/Pk-syF1DD6FOFA2gry2dxRVfJCI\"",
+    "id": {
+     "kind": "youtube#video",
+     "videoId": "H4XrsYKMHl4"
+    },
+    "snippet": {
+     "publishedAt": "2018-06-14T16:00:11.000Z",
+     "channelId": "UCYCwDl5Cu_QnS-Hm76TnU5Q",
+     "title": "Overwatch Moments #148",
+     "description": "Support us on Patreon: https://www.patreon.com/rapida Submit your plays: rapidatv@gmail.com Subscribe, like and favorite! Join me at social networks: ...",
+     "thumbnails": {
+      "default": {
+       "url": "https://i.ytimg.com/vi/H4XrsYKMHl4/default.jpg",
+       "width": 120,
+       "height": 90
+      },
+      "medium": {
+       "url": "https://i.ytimg.com/vi/H4XrsYKMHl4/mqdefault.jpg",
+       "width": 320,
+       "height": 180
+      },
+      "high": {
+       "url": "https://i.ytimg.com/vi/H4XrsYKMHl4/hqdefault.jpg",
+       "width": 480,
+       "height": 360
+      }
+     },
+     "channelTitle": "Rapida",
+     "liveBroadcastContent": "none"
+    }
+   }
+  ]
+ }
+
+
 
 /*
   We want our store to hold a `videos` array of "decorated" objects - i.e. objects that
@@ -18,7 +193,7 @@ const store = {
 
 // TASK: Add the Youtube Search API Base URL here:
 // Documentation is here: https://developers.google.com/youtube/v3/docs/search/list#usage
-const BASE_URL = '';
+const BASE_URL = `https://www.googleapis.com/youtube/v3/search`;
 
 // TASK:
 // 1. Create a `fetchVideos` function that receives a `searchTerm` and `callback`
@@ -26,7 +201,16 @@ const BASE_URL = '';
 // 3. Make a getJSON call using the query object and sending the provided callback in as the last argument
 // TEST IT! Execute this function and console log the results inside the callback.
 const fetchVideos = function(searchTerm, callback) {
-
+  // get value of input
+  console.log('fetchVideos function is working.');
+  // const url = BASE_URL + searchTerm;
+  const query = {
+    part: 'snippet',
+    key: API_KEY,
+    q: searchTerm,
+    maxResults: 5
+  };
+  $.getJSON(BASE_URL, query, callback);
 };
 
 // TASK:
@@ -39,6 +223,14 @@ const fetchVideos = function(searchTerm, callback) {
 // you get back the object you want.
 const decorateResponse = function(response) {
 
+  console.log('It is running.');
+
+  return response.items.map(item => ({
+      id: item.id.videoId,
+      title: item.snippet.title,
+      thumbnail: item.snippet.thumbnails.medium.url,
+  }));
+  console.log(results);
 };
 
 // TASK:
@@ -46,7 +238,13 @@ const decorateResponse = function(response) {
 // 2. Using the object, return an HTML string containing all the expected data
 // TEST IT!
 const generateVideoItemHtml = function(video) {
-
+  return `
+  <li data-id="${video.id}">
+  ${video.id}
+    <h3>${video.title}</h3>
+    <a href="https://www.youtube.com/watch?v=${video.id}"><img src="${video.thumbnail}"></a>
+  </li>
+  `;
 };
 
 // TASK:
@@ -54,7 +252,7 @@ const generateVideoItemHtml = function(video) {
 // objects and sets the array as the value held in store.videos
 // TEST IT!
 const addVideosToStore = function(videos) {
-
+    store.videos = videos;
 };
 
 // TASK:
@@ -63,8 +261,16 @@ const addVideosToStore = function(videos) {
 // 3. Add your array of DOM elements to the appropriate DOM element
 // TEST IT!
 const render = function() {
-
+  const videoElements = store.videos.map(data => {
+    return generateVideoItemHtml(data);
+  })
+  $('.results').html(videoElements)
 };
+
+// const videos = decorateResponse(mockData);
+// addVideosToStore(videos);
+// render();
+
 
 // TASK:
 // 1. Create a `handleFormSubmit` function that adds an event listener to the form
@@ -78,11 +284,26 @@ const render = function() {
 //   g) Inside the callback, run the `render` function 
 // TEST IT!
 const handleFormSubmit = function() {
+  $('form').on('submit', function (event) {
+    event.preventDefault();
+    console.log('Clicked!');
+    const searchVal = $(event.currentTarget).find('#search-term').val();
+    console.log(searchVal);
+    $('#search-term').val('');
 
+    fetchVideos(searchVal, function(response) {
+      console.log(response);
+      const videos = decorateResponse(response);
+      addVideosToStore(videos);
+      render();
+    });
+  });
 };
+
 
 // When DOM is ready:
 $(function () {
   // TASK:
   // 1. Run `handleFormSubmit` to bind the event listener to the DOM
+  handleFormSubmit();
 });
